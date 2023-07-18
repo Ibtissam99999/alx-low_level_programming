4f0c73943b1b6443fe57706d0c008749ca726181
Ibtissam99999 <ibtissamsahmoudi0@gmil.com>
2023-07-18 03:32:56 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Sucess)
+ */
+
+int main(void)
+{
+	char *h = "Holberton\n";
+
+	while (*h)
+	{
+		_putchar(*h);
+		h++;
+	}
+	return (0);
+}
